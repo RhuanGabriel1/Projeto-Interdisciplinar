be998a6cd631a80007ac164ff186615c8eeca237
safrapc <leandrosafra.jconectada@gmail.com>
2021-10-11 14:40:39 -0300

    fix2

---
+++ API/src/server.js
@@ -0,0 +1,27 @@
+const mysql = require("mysql2/promise");
+const express = require("express");
+const cors = require("cors");
+const router = require("./routes");
+const app = express();
+
+(async () => {
+  app.use(express.json());
+  app.use(cors());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(router);
+
+  const connection = await mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "admin",
+    port: "3306",
+    database: "testedb",
+  });
+  console.clear();
+  global.con = connection;
+  console.log("Conectou no MySQL!");
+
+  app.listen(4000, function () {
+    console.log("Server is running on port 4000");
+  });
+})();
