a8e18e8411dbffeecb04038564bc82435c862f19
Eduardo <42119808+eduardodini@users.noreply.github.com>
2021-10-27 17:40:29 -0300

    Revisão das propriedades e métodos adequando-os ao clean code

--- Codigo/app/src/main/java/com/example/imed/Controllers/Medicamentos.java
+++ Codigo/app/src/main/java/com/example/imed/Controllers/Medicamentos.java
@@ -2,65 +2,71 @@ package com.example.imed.Controllers;
 
 public class Medicamentos {
 
-    private String catmat, pr_ativo, concentracao, fornecimento, forma_farm,nome_medicamento,fk_idReceita,fk_crm_med,fk_crf_farm;
-
-
-    public void setCatmat(String catmat) {
-        this.catmat = catmat;
-    }
-
-    public void setPr_ativo(String pr_ativo) {
-        this.pr_ativo = pr_ativo;
-    }
-
-    public void setConcentracao(String concentracao) {
-        this.concentracao = concentracao;
-    }
-
-    public void setFornecimento(String fornecimento) {
-        this.fornecimento = fornecimento;
-    }
-
-    public void setForma_farm(String forma_farm) {
-        this.forma_farm = forma_farm;
-    }
-
-    public void setNome_medicamento(String nome_medicamento) { this.nome_medicamento = nome_medicamento; }
-
-    public void setFk_crf_farm(String fk_crf_farm) {
-        this.fk_crf_farm = fk_crf_farm;
-    }
-
-    public String getFk_crf_farm() {
-        return fk_crf_farm;
-    }
-
-    public String getNome_medicamento() {
-        return nome_medicamento;
-    }
+//    private String catmat, pr_ativo, concentracao, fornecimento, forma_farm,nome_medicamento,fk_idReceita,fk_crm_med,fk_crf_farm;
+//    deixei as partes referentes ao banco de dados de fora, repensar futuramente
+    private String catmat;
+    private String principioAtivo;
+    private String concentracao;
+    private String fornecedor;
+    private String  formaFarmaceutica;
+    private String nomeMedicamento;
+    private Farmaceutico farmaceutico;
 
     public String getCatmat() {
         return catmat;
     }
 
-    public String getPr_ativo() {
-        return pr_ativo;
+    public void setCatmat(String catmat) {
+        this.catmat = catmat;
+    }
+
+    public String getPrincipioAtivo() {
+        return principioAtivo;
+    }
+
+    public void setPrincipioAtivo(String principioAtivo) {
+        this.principioAtivo = principioAtivo;
     }
 
     public String getConcentracao() {
         return concentracao;
     }
 
-    public String getFornecimento() {
-        return fornecimento;
+    public void setConcentracao(String concentracao) {
+        this.concentracao = concentracao;
     }
 
-    public String getForma_farm() {
-        return forma_farm;
+    public String getFornecedor() {
+        return fornecedor;
     }
 
+    public void setFornecedor(String fornecedor) {
+        this.fornecedor = fornecedor;
+    }
 
+    public String getFormaFarmaceutica() {
+        return formaFarmaceutica;
+    }
 
+    public void setFormaFarmaceutica(String formaFarmaceutica) {
+        this.formaFarmaceutica = formaFarmaceutica;
+    }
+
+    public String getNomeMedicamento() {
+        return nomeMedicamento;
+    }
+
+    public void setNomeMedicamento(String nomeMedicamento) {
+        this.nomeMedicamento = nomeMedicamento;
+    }
+
+    public Farmaceutico getFarmaceutico() {
+        return farmaceutico;
+    }
+
+    public void setFarmaceutico(Farmaceutico farmaceutico) {
+        this.farmaceutico = farmaceutico;
+    }
 
     @Override
     public  String toString(){
@@ -68,4 +74,6 @@ public class Medicamentos {
                 "\nPrincípio ativo: " + getPr_ativo() + "\nConcentração: " + getConcentracao() +
                 "\nFornecimento: " + getFornecimento() + "\nForma Farmacêutica: " + getForma_farm();
     }
+
+
 }
